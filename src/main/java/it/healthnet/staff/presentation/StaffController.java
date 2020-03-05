d6db250d270da934215e49a67e11656b86e17a24
Sbyner <tmmscrltt@gmail.com>
2020-03-05 22:01:43 +0100

    [FIX] - Typo

--- src/main/java/it/healthnet/staff/presentation/StaffController.java
+++ src/main/java/it/healthnet/staff/presentation/StaffController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
-@RequestMapping("/staff")
+@RequestMapping("/users")
 public final class StaffController {
     private final StaffService staffService;
 
